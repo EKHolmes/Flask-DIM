6b66e428969c538274989fb41190ffcd99159f8f
Erik Holmes <60955016+EKHolmes@users.noreply.github.com>
2023-01-20 23:08:05 +0000

    Is able to collect the auth code for the user and save it. Fucking finally.

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-from flask import Flask, request, make_response, render_template
+from flask import Flask, request, make_response, render_template, redirect
 import essvar
 
 app = Flask(__name__)
 
 
 @app.route('/', methods = ["GET", "POST"])
-def index():
+def landing():
     code = request.cookies.get("code")
     if code is None:
         return render_template("authorize.html")
     else:
-        return code
+        return redirect("/index")
 
 
 
@@ -20,5 +20,12 @@ def receivePost():
     userCode = request.args["code"]
     returnResp = make_response(render_template('baseIndex.html'))
     returnResp.set_cookie('code', userCode)
-    return returnResp
+    return redirect("/", code=302, Response=returnResp)
 app.run(host='0.0.0.0', port=81)
+
+
+
+
+@app.route('/index')
+def index():
+    return "you made it"
