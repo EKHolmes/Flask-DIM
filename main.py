5c7812e1b4b1367d76a757df4e9fa6be6802f0f1
Erik Holmes <60955016+EKHolmes@users.noreply.github.com>
2023-01-20 21:49:33 +0000

    Added css for auth button, link, and header. Having issues with receiving the code and the redirect after it.

--- main.py
+++ main.py
@@ -10,16 +10,15 @@ def index():
     if code is None:
         return render_template("authorize.html")
     else:
-        return render_template("baseIndex.html")
+        return code
 
 
 
 
-@app.route('/receiver', methods = ["POST"])
+@app.route('/receiver', methods = ["GET", "POST"])
 def receivePost():
-    if request.method == "POST":
-        userCode = request.form["code"]
-        returnResp = make_response(render_template('baseIndex.html'))
-        returnResp.set_cookie('code', userCode)
+    userCode = request.args["code"]
+    returnResp = make_response(render_template('baseIndex.html'))
+    returnResp.set_cookie('code', userCode)
     return returnResp
 app.run(host='0.0.0.0', port=81)
